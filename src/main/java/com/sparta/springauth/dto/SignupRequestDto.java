78be4568c95f40c981780f20a1e8df13672ed241
Sona <objet_an@naver.com>
2025-09-10 17:14:34 +0900

    이메일 형식 지정 (@pattern)

--- src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta.springauth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,11 +11,17 @@ import lombok.Setter;
 public class SignupRequestDto {
     @NotBlank
     private String username;
+
     @NotBlank
     private String password;
-    @Email
-    @NotBlank
+
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",
+            message = "올바른 이메일 주소 형식이 아닙니다."
+    )
+    @NotBlank(message = "이메일은 필수 입력값입니다")
     private String email;
+
     private boolean admin = false;
+
     private String adminToken = "";
 }
